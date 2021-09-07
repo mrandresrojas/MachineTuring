cccabf1b6ace980a25b2e94d8c7b8d4304d028b4
Andres Rojas <andresrojasjimenez@hotmail.com>
2021-09-06 21:58:35 -0500

    set variables and function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'MachineTuring';
+  value = '';
+  programa_turing="0 _ 1 r 1,1 _ 1 r 2,2 _ 1 r 3";
+  estados_acept="3";
+  test = this.programa_turing.split(",",100);
+  estados_aceptacion=this.estados_acept.split(",",100)
+  cinta=["_","_","_","_","_","_","_","_","_"];
+  sentencias=this.programa_turing.split(",",100)
+
+mostrar(){
+console.log(this.test)
+
+
+}
+MachineTuring(){
+  for (var i = 0; i < this.sentencias.length; i++){
+    var states[] = (this.sentencias[i]);
+
+  }
+}
+
+
 }
